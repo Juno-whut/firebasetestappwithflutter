2d65903d9101e6c2cd58a16e8a980a21168e11c2
Juno-whut <owenrupnow17@gmail.com>
2024-10-24 16:53:30 -0400

    added in some for firebase stuff and add made the firestore db and changed the rules to allow read and writes for development and testing

--- firebasenotes/lib/main.dart
+++ firebasenotes/lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebasenotes/firebase_options.dart';
+// import 'package:firebasecrudtute/firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
