095622b2199d00d98e7ce52d259f68ccdee6ecb5
Juno-whut <owenrupnow17@gmail.com>
2024-10-25 15:37:06 -0400

    made a services folder for firestore.dart which is responsible for CRUD activities on the db

---
+++ firebasenotes/lib/services/firestore.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class FirestoreService {
+
+  // get collection of notes from db
+  final CollectionReference notes = FirebaseFirestore.instance.collection('notes');
+
+  // Create: add new note
+  Future<void> addNote(String note) {
+    return notes.add(
+      {'note': note,
+      'timestap': Timestamp.now()}
+    );
+  }
+ 
+  // Read: get notes from db
+
+  // Update: update notes in db given doc id
+
+  // Delete: delete notes from db given doc id
+}
